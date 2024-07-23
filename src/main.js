ecb60d1999df5325946318d7eeb6d993d5cf3d64
Ranjit1124 <ranjitpr1124@gmail.com>
2024-07-23 12:01:56 +0530

    navigation

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+import 'popper.js';
+import 'jquery';
 
 createApp(App).mount('#app')
